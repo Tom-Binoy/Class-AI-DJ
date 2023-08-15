0765f4a5df8af75f4f4b998db14a0e0c996f174e
Tom-Binoy <111070900+Tom-Binoy@users.noreply.github.com>
2023-08-15 15:08:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ rwristx = 0;
 lwristx = 0;
 rwristy = 0;
 lwristy = 0;
-score = 0;
+scorel = 0;
+scorer = 0;
 
 
 function preload(){
@@ -26,12 +27,17 @@ function draw(){
     image(video, 0, 0, 700, 500);
     fill("White");
     stroke("blue");
-    if(score >= 0.3){
+    if(scorel >= 0.3){
         circle(lwristx,lwristy,20);
         d = floor(Number(lwristy))
         result = d/500;
         music.setVolume(result);
         document.getElementById("volume").innerHTML = "Volume : "+result;
+    }
+    else if(scorer >= 0.3 )
+    {
+        circle(rwristx,rwristy,20);
+        checkrw()
     }
 }
 function play()
@@ -51,6 +57,28 @@ function gotPoses(result)
         rwristy = rw.y;
         lwristx = lw.x;
         lwristy = lw.y;
-        score = w.keypoints[9].score
+        scorel = w.keypoints[9].score
+        scorer = w.keypoints[10].score
     }
+}
+function checkrw()
+{   no = 0;  w = rwristy;
+    if( w >= 0 && w<= 100){
+        no = 0.5;
+    }
+    else if( w >= 101 && w<= 200){
+        no = 1;
+    }
+    else if( w >= 201 && w<= 300){
+        no = 1.5;
+    }
+    else if( w >= 301 && w<= 400){
+        no = 2;
+    }
+    else if( w >= 401 && w<= 500){
+        no = 2.5;
+    }
+
+    document.getElementById("speed").innerHTML = "Speed : "+no+"x";
+    music.rate(no)
 }
